daf41baacdd81f41ce98a4bda62a6efdbcb8ded1
Austin Strom <austinjstrom@gmail.com>
2022-02-02 15:14:51 -0600

    Fixing syntax error

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function listCratesSync(seratoFolders = [PLATFORM_DEFAULT_SERATO_FOLDER]) {
         return new Crate(name, seratoFolder);
       } else if(path.basename(x) === "Videos" && fs.statSync(path.join(subcratesFolder, x)).isDirectory()){
         console.log("Found video folder");
-        const videoCrates = fs.readdirSync(path.join(subcratesFolder, x).map((elem) => {
+        const videoCrates = fs.readdirSync(path.join(subcratesFolder, x)).map((elem) => {
           console.log("Found video crate with name " + x);
           const name = path.basename(x, ".crate");
           return new Crate(name, seratoFolder);
